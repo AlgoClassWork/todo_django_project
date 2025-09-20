034c43957e3c1eb7bbd2fb3b54f37d607937d0fc
AlgoClassWork <kyxec.play@gmail.com>
2025-09-20 10:43:04 +0600

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -4,5 +4,7 @@ from .models import Task
 
 # Create your views here.
 def home(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-created_at')
     return render(request, 'home.html', {'tasks' : tasks})
+
+
